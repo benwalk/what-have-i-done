22619b424b380e0cb8071bab95c3751b2467c67d
benwalker <benwalker@simple.com>
2020-08-28 18:47:12 -0700

    Updates

---
+++ precache-manifest.058f1caafb9b732c4525e22662fe1d62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be88f82066cc0c0865ac177d6696aa1",
+    "url": "/what-have-i-done/index.html"
+  },
+  {
+    "revision": "e5f08cf673f5fb1dddc2",
+    "url": "/what-have-i-done/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26cbf3d0b1304db62c75",
+    "url": "/what-have-i-done/static/js/2.194bd562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/what-have-i-done/static/js/2.194bd562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f08cf673f5fb1dddc2",
+    "url": "/what-have-i-done/static/js/main.c5874646.chunk.js"
+  },
+  {
+    "revision": "fe6db47708705bcf2a93",
+    "url": "/what-have-i-done/static/js/runtime-main.b49e9b0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/what-have-i-done/static/media/logo.5d5d9eef.svg"
+  }
+]);
